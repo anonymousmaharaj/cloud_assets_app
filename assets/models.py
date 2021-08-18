7550b418a9ad9a10b76e4f545030f6ade55f482c
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-08-18 14:31:58 +0300

    Fixed 1st commit. Add req.txt

---
+++ assets/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class File(models.Model):
+    ...
+
+
+
+class Folder(models.Model):
+    ...
--- assets/views.py
+++ assets/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 
 
 # Create your views here.
 def health_check(request):
-    return JsonResponse({'server_status': '200'})
+    return JsonResponse({'server_status': 200})
