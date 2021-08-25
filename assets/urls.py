83a1ec9f266f987144545e7355545873ff202669
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-08-25 17:45:15 +0300

    Add auth feature

--- assets/urls.py
+++ assets/urls.py
@@ -1,8 +1,10 @@
-from assets.views import health_check, show_page
+from assets.views import health_check, show_page, user_login, user_logout
 
 from django.urls import path
 
 urlpatterns = [
     path('api/health_check/', health_check),
     path('', show_page, name='root_page'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout')
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,7 +1,9 @@
 from assets.db.queries import get_assets_list
+from assets.forms import UserLoginForm
 
+from django.contrib.auth import login, logout
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def health_check(request):
@@ -12,3 +14,21 @@ def show_page(request):
     rows = get_assets_list(None)
     context = {'rows': rows}
     return render(request, 'assets/root_page.html', context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('root_page')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'assets/login.html', context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
