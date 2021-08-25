65c0d2bbb9c7b537d54c2e5299ae4e9e1213bfcc
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-08-25 15:51:58 +0300

    Add validators for get requests.

--- assets/urls.py
+++ assets/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('api/health_check/', health_check),
-    path('', show_page, name='root_page'),
+    path('', show_page, name='root_page')
 ]
--- common/validators.py
+++ common/validators.py
@@ -1,5 +1,16 @@
-def validate_int(param):
-    if param.isdigit() and int(param) > 0:
-        return int(param)
-    else:
-        return None
+from django.http import HttpResponseBadRequest
+
+
+def validate_folder_id(folder_id):
+    if folder_id is not None:
+        if not folder_id.isdigit():
+            return HttpResponseBadRequest
+    return 200
+
+
+def validate_get_params(params):
+    accept_params = ['folder']
+    for param in params:
+        if param not in accept_params:
+            return HttpResponseBadRequest
+    return 200
