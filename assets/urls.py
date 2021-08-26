3325c7e22b1e3ef9a165c8bcb594a5efe6cabd28
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-08-26 11:00:24 +0300

    Add register

--- assets/urls.py
+++ assets/urls.py
@@ -1,8 +1,10 @@
-from assets.views import health_check, show_page
+from assets.views import health_check, show_page, user_register
 
 from django.urls import path
+
 
 urlpatterns = [
     path('api/health_check/', health_check),
     path('', show_page, name='root_page'),
+    path('register/', user_register, name='register')
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,7 +1,9 @@
 from assets.db.queries import get_assets_list
+from assets.forms import UserRegisterForm
 
+from django.contrib import messages
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def health_check(request):
@@ -12,3 +14,18 @@ def show_page(request):
     rows = get_assets_list(None)
     context = {'rows': rows}
     return render(request, 'assets/root_page.html', context)
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Success!')
+            return redirect('root_page')
+        else:
+            messages.error(request, 'Error!')
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'assets/register.html', context=context)
