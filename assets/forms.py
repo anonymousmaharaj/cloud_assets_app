4a29e9f6d73885f888b0203e0a8dbc14502615a1
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-08-27 16:28:35 +0000

    Update forms.py

--- assets/forms.py
+++ assets/forms.py
@@ -1,18 +1,15 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-
-class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Username')
-    password = forms.CharField(label='Password')
-
-
 """Customs forms for Assets app."""
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
+
+class UserLoginForm(AuthenticationForm):
+    """Basic class for login."""
+
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password')
 
 class UserRegisterForm(UserCreationForm):
     """Custom form for registration."""
