df81029f8648f5453a9c6fd457a029ada4ca0cd7
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-09-21 11:32:57 +0300

    Fix url for api

--- assets/urls.py
+++ assets/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('move/', views.move_file, name='move_file'),
     path('rename-file/', views.rename_file, name='rename_file'),
     path('assets/folder/<int:folder_id>/rename/', views_v2.RenameFolderView.as_view()),
-    path('api/assets/folder/<int:pk>/update', views_v2.FolderUpdate.as_view())
+    path('api/assets/folder/<int:pk>', views_v2.FolderUpdate.as_view())
 ]
