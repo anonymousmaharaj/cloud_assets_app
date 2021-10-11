7eed70da1a1a120a9f64d08539cce99fe54952b8
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-10-11 12:42:10 +0300

    Fix logging and env.

--- cloud_assets/settings.py
+++ cloud_assets/settings.py
@@ -1,10 +1,6 @@
 import os
 
 import logging
-import environ
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -105,44 +101,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cloud_assets/static'),
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'formatter': {
-            'format': '{asctime} [{levelname}] [{lineno}] [{module}] {message}',
-            'style': '{'
-        }
-    },
-    'handlers': {
-        'assets_views': {
-            'level': 'WARNING',
-            'class': 'logging.FileHandler',
-            'filename': os.getenv('ASSETS_VIEW_LOG_FILE'),
-            'formatter': 'formatter',
-            'encoding': 'utf-8'
-        },
-        'requests': {
-            'level': 'WARNING',
-            'class': 'logging.FileHandler',
-            'filename': os.getenv('REQUESTS_LOG_FILE'),
-            'formatter': 'formatter',
-            'encoding': 'utf-8'
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['requests'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'assets': {
-            'handlers': ['assets_views'],
-            'level': 'WARNING',
-            'propagate': True,
-        }
-    },
-}
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'assets.utils.custom_exception_handler',
