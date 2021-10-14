76074ba9ea4bf519e65b59b5de0c1a7fcf6a1699
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-10-14 12:41:28 +0300

    Fix permissions.

--- assets/views_v2.py
+++ assets/views_v2.py
@@ -334,15 +334,14 @@ class FolderRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     """View for rename folder's endpoint."""
 
     queryset = models.Folder.objects.all()
-    permission_classes = (permissions.IsObjectOwner, permissions.IsParentOwner, IsAuthenticated)
+    permission_classes = (permissions.IsObjectOwner, IsAuthenticated)
     serializer_class = serializers.FolderRetrieveUpdateSerializer
 
 
 class FolderListCreateView(generics.ListCreateAPIView):
     """Generic APIView for List and Create folders."""
 
-    queryset = models.Folder.objects.all()
-    permission_classes = (permissions.IsParentOwner, IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
     serializer_class = serializers.FolderListCreateSerializer
     parser_classes = (JSONParser,)
 
@@ -352,22 +351,33 @@ class FolderListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         """Override this method to save additional fields."""
+        serializer.is_valid(raise_exception=True)
+        parent = serializer.validated_data.get('parent')
+        if parent is not None:
+            if not self.request.user.folders.filter(pk=parent.pk):
+                raise PermissionDenied(detail='You do not have permission to perform this action.')
         serializer.save(owner=self.request.user)
 
 
 class FileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    """View for rename folder's endpoint."""
+    """View for rename file's endpoint."""
 
     queryset = models.File.objects.all()
-    permission_classes = (permissions.IsObjectOwner, permissions.IsFolderOwner, IsAuthenticated)
+    permission_classes = (permissions.IsObjectOwner, IsAuthenticated)
     serializer_class = serializers.FileRetrieveUpdateDestroySerializer
+
+    def perform_update(self, serializer):
+        serializer.is_valid(raise_exception=True)
+        folder = serializer.validated_data.get('folder')
+        if folder is not None:
+            if not self.request.user.folders.filter(pk=folder.pk):
+                raise PermissionDenied(detail='You do not have permission to perform this action.')
 
 
 class FileListCreateView(generics.ListCreateAPIView):
     """Generic APIView for List and Create files."""
 
-    queryset = models.File.objects.all()
-    permission_classes = (permissions.IsFolderOwner, IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
     serializer_class = serializers.FileListCreateSerializer
 
     def get_queryset(self):
@@ -376,9 +386,20 @@ class FileListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         """Override this method to save additional fields."""
+        serializer.is_valid(raise_exception=True)
+        folder = serializer.validated_data.get('folder')
+        if folder is not None:
+            if not self.request.user.folders.filter(pk=folder.pk):
+                raise PermissionDenied(detail='You do not have permission to perform this action.')
+
         rk = create_file_relative_key(self.request.user.pk)
+        extension = os.path.splitext(serializer.validated_data.get('title'))[1]
+        size = serializer.validated_data.get('size')
+
         serializer.save(owner=self.request.user,
-                        relative_key=rk)
+                        relative_key=rk,
+                        extension=extension,
+                        size=size)
 
 
 class ShareListCreateView(generics.ListCreateAPIView):
