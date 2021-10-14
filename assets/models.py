67e421444b648eeca16e729bf6965ef7bbae4e49
Alexey Fateyev <alexey.fateyev@dataart.com>
2021-10-14 12:44:18 +0300

    Fix model's clean method.

--- assets/models.py
+++ assets/models.py
@@ -97,8 +97,6 @@ class Folder(models.Model):
         """Check exist folder with same title."""
         if Folder.objects.filter(title=self.title, owner=self.owner, parent=self.parent).first():
             raise ValidationError('Current folder already exists.')
-        if self == self.parent:
-            raise ValidationError('Cannot move folder in itself.')
 
 
 class Permissions(models.Model):
